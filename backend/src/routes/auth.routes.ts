ef6fa74a9f1dc0d1cd26eac96a6ba621a0c8b3fd
diwasbk <diwasbk903@gmail.com>
2026-02-14 00:27:54 +0545

    feat(user-route): Add user router and connect getAllUser controller

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import AuthController from "../controllers/auth.controller";
+import schemaValidatewareMiddleware from "../middlewares/schema.validator.middleware";
+import { userSchema } from "../types/user.types";
 
 const authRouter = express.Router();
 const authController = new AuthController();
 
-authRouter.post("/signup", authController.signupUser);
+authRouter.post("/signup", schemaValidatewareMiddleware(userSchema), authController.signupUser);
 authRouter.post("/login", authController.loginUser);
 
 export default authRouter;
