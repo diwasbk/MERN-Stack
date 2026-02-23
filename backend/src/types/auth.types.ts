3a55ad436385ac8fec353e0d063f0fac7bf1fc9e
diwasbk <diwasbk903@gmail.com>
2026-02-23 11:50:41 +0545

    feat(validation): Add login and change password schemas using zod

---
+++ backend/src/types/auth.types.ts
@@ -0,0 +1,17 @@
+import z from "zod";
+
+/* Login Schema */
+export const loginSchema = z.object({
+    email: z.string("Email is required.").nonempty("Email is required.").email({ message: "Please enter a valid email address." }),
+    password: z.string("Password is required.").nonempty("Password is required.")
+});
+
+/* Change Password Schema */
+export const changePasswordSchema = z.object({
+    currentPassword: z.string("Current Password is required.").nonempty("Current Password is required."),
+    newPassword: z.string("New Password is required.").nonempty("New Password is required.").min(6, "Password must be at least 6 characters."),
+    confirmPassword: z.string("Confirm Password is required.").nonempty("Confirm Password is required."),
+}).refine((data) => data.newPassword === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"]
+});
